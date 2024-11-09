97574ed407cbdfb391a79aebc140fea3a77f4c26
Kendorex <134404509+Kendorex@users.noreply.github.com>
2024-11-09 17:32:03 +0800

    Initialize Git repository and add large PDF file using Git LFS

---
+++ ddd.js
@@ -0,0 +1,145 @@
+document.addEventListener('click', e => {
+    if (e.target.matches('a[id]')){
+        document.getElementById('section').textContent=""
+        document.getElementById('iframe').width="100%"
+        document.getElementById('iframe').height="100%"
+        
+      if (e.target.id == 'history7'){
+        document.getElementById('iframe').src="history7.pdf"
+      } 
+      if (e.target.id == 'history8'){
+        document.getElementById('iframe').src="histoty8.pdf"
+      }
+      if (e.target.id == 'history9'){
+        document.getElementById('iframe').src="history9.pdf"
+      } 
+      if (e.target.id == 'algebra7'){
+        document.getElementById('iframe').src="https://drive.google.com/file/d/1MASsuLHFPPuZsknBbUzPU7Oegei537YI/view?usp=sharing"
+      } 
+      if (e.target.id == 'algebra8'){
+        document.getElementById('iframe').src="algebra8.pdf"
+      } 
+      if (e.target.id == 'algebra9'){
+        document.getElementById('iframe').src="algebra9.pdf"
+      }
+      if (e.target.id == 'russian7'){
+        document.getElementById('iframe').src="russian7.pdf"
+      } 
+      if (e.target.id == 'russian8'){
+        document.getElementById('iframe').src="russian8.pdf"
+      }
+      if (e.target.id == 'russian9'){
+        document.getElementById('iframe').src="russian9.pdf"
+      } 
+      if (e.target.id == 'fisika7'){
+        document.getElementById('iframe').src="fisika7.pdf"
+      } 
+      if (e.target.id == 'fisika8'){
+        document.getElementById('iframe').src="fisika8.pdf"
+      } 
+      if (e.target.id == 'fisika9'){
+        document.getElementById('iframe').src="fisika9.pdf"
+      }
+      if (e.target.id == 'geometry7'){
+        document.getElementById('iframe').src="geometry7.pdf"
+      } 
+      if (e.target.id == 'geometry8'){
+        document.getElementById('iframe').src="geometry8.pdf"
+      } 
+      if (e.target.id == 'geometry9'){
+        document.getElementById('iframe').src="geometry9.pdf"
+      }
+      if (e.target.id == 'informatics7'){
+        document.getElementById('iframe').src="informatics7.pdf"
+      } 
+      if (e.target.id == 'informatics8'){
+        document.getElementById('iframe').src="informatics8.pdf"
+      } 
+      if (e.target.id == 'informatics9'){
+        document.getElementById('iframe').src="informatics9.pdf"
+      }
+      if (e.target.id == 'himia8'){
+        document.getElementById('iframe').src="himia8.pdf"
+      } 
+      if (e.target.id == 'himia9'){
+        document.getElementById('iframe').src="himia9.pdf"
+      }
+      if (e.target.id == 'english7'){
+        document.getElementById('iframe').src="english7.pdf"
+      } 
+      if (e.target.id == 'english8'){
+        document.getElementById('iframe').src="english8.pdf"
+      } 
+      if (e.target.id == 'english9'){
+        document.getElementById('iframe').src="english9.pdf"
+      }
+      if (e.target.id == 'geografia7'){
+        document.getElementById('iframe').src="geografia7.pdf"
+      } 
+      if (e.target.id == 'geografia8'){
+        document.getElementById('iframe').src="geografia8.pdf"
+      } 
+      if (e.target.id == 'geografia9'){
+        document.getElementById('iframe').src="geografia9.pdf"
+      }
+      if (e.target.id == 'russian7'){
+        document.getElementById('iframe').src="russian7.pdf"
+      } 
+      if (e.target.id == 'russian8'){
+        document.getElementById('iframe').src="russian8.pdf"
+      } 
+      if (e.target.id == 'russian9'){
+        document.getElementById('iframe').src="russian9.pdf"
+      }
+      if (e.target.id == 'fizika7'){
+        document.getElementById('iframe').src="fizika7.pdf"
+      } 
+      if (e.target.id == 'fizika8'){
+        document.getElementById('iframe').src="fizika8.pdf"
+      } 
+      if (e.target.id == 'fizika9'){
+        document.getElementById('iframe').src="fizika9.pdf"
+      }
+      if (e.target.id == 'biologi7'){
+        document.getElementById('iframe').src="biologi7.pdf"
+      } 
+      if (e.target.id == 'biologi8'){
+        document.getElementById('iframe').src="biologi8.pdf"
+      } 
+      if (e.target.id == 'biologi9'){
+        document.getElementById('iframe').src="biologi9.pdf"
+      }
+      if (e.target.id == 'literatyre7.1'){
+        document.getElementById('iframe').src="literatyre7.1.pdf"
+      }
+      if (e.target.id == 'literatyre7.2'){
+        document.getElementById('iframe').src="literatyre7.2.pdf"
+      }
+      if (e.target.id == 'literatyre8.1'){
+        document.getElementById('iframe').src="literatyre8.1.pdf"
+      }
+      if (e.target.id == 'literatyre8.2'){
+        document.getElementById('iframe').src="literatyre8.2.pdf"
+      }
+      if (e.target.id == 'literatyre9.1'){
+        document.getElementById('iframe').src="literatyre9.1.pdf"
+      }
+      if (e.target.id == 'literatyre9.2'){
+        document.getElementById('iframe').src="literatyre9.2.pdf"
+      }
+    }
+  });
+
+  document.addEventListener('click', e => {
+    if (e.target.matches('span[id]')){
+      document.getElementById('iframe').width="0%"
+      document.getElementById('iframe').height="0%"
+      document.getElementById('section').textContent="Добро пожаловать в электронную библиотеку Зырянова Андрея! Наша библиотека создана с целью предоставить учащимся и преподавателям доступ к разнообразным учебным материалам, необходимым для успешного освоения школьной программы.Мы стремимся сделать обучение более доступным и интересным, предоставляя пользователям качественные и актуальные материалы.Спасибо за внимание! Мы надеемся, что наша библиотека станет для вас ценным ресурсом на вашем образовательном пути."
+    }});
+    document.addEventListener('click', e => {
+        if (e.target.matches('i[id]')){
+          document.getElementById('iframe').width="0%"
+          document.getElementById('iframe').height="0%"
+          document.getElementById('section').textContent="Добро пожаловать в электронную библиотеку Зырянова Андрея! Наша библиотека создана с целью предоставить учащимся и преподавателям доступ к разнообразным учебным материалам, необходимым для успешного освоения школьной программы.Мы стремимся сделать обучение более доступным и интересным, предоставляя пользователям качественные и актуальные материалы.Спасибо за внимание! Мы надеемся, что наша библиотека станет для вас ценным ресурсом на вашем образовательном пути."
+        }
+  });
